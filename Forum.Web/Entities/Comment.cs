b2a9d43e714a547639eb14960afc8c333052f381
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-09 11:16:37 +0200

    add Comment entity

---
+++ Forum.Web/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+
+        public Post Post { get; set; }
+
+        public User User { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
