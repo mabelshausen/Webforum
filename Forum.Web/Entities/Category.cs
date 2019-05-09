dee62a8037859e78d337ec2112ab85aa8bdb65fe
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-09 11:11:48 +0200

    add Category entity

---
+++ Forum.Web/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public Theme Theme { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
