2f6157a901fc2c2791074d66f0ac152e7c0d8e68
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-09 11:15:06 +0200

    add Post entity

---
+++ Forum.Web/Entities/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Entities
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public User User { get; set; }
+
+        public Category Category { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
