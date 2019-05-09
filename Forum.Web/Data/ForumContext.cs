83c1bbc2b052238e3664f6f514601c257e7f483a
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-09 11:22:03 +0200

    add ForumContext

---
+++ Forum.Web/Data/ForumContext.cs
@@ -0,0 +1,25 @@
+﻿using Forum.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Data
+{
+    public class ForumContext : DbContext
+    {
+        public ForumContext(DbContextOptions<ForumContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
