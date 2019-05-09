3c4d2f27bbd51491dc5a28450e89c841fdbe0886
Kasperdee <kasperdee@hotmail.com>
2019-05-09 11:11:34 +0200

    Add LoginController and it's Views and ViewModels.

---
+++ Forum.Web/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
