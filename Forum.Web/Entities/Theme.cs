e43d72eb15cdfd12174624dcd1f074a328cf34a7
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-09 11:06:36 +0200

    add Theme entity

---
+++ Forum.Web/Entities/Theme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Entities
+{
+    public class Theme
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
