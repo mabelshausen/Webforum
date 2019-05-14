96c078ced889d276dea7153f1dc44f074947119f
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-14 16:20:33 +0200

    add postscontroller + index view + vm

---
+++ Forum.Web/Controllers/PostsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Web.Controllers
+{
+    public class PostsController : Controller
+    {
+        private ForumContext fc;
+
+        public PostsController (ForumContext context)
+        {
+            fc = context;
+        }
+
+        public IActionResult Index(string theme, string category)
+        {
+            var model = new PostsIndexVm();
+            return View(model);
+        }
+    }
+}
