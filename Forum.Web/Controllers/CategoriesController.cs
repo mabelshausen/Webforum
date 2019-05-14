ad8cb14dc22a2f8169d0135b2a700c78eecd6fa8
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-14 15:44:01 +0200

    sepatate categories by theme in view

--- Forum.Web/Controllers/CategoriesController.cs
+++ Forum.Web/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Forum.Web.Entities;
 using Forum.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,11 +17,14 @@ namespace Forum.Web.Controllers
             fc = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string theme)
         {
             var model = new CategoriesIndexVm();
 
             model.Categories = fc.Categories;
+            model.CategoriesBytheme = fc.Categories
+                .Where(c => c.Theme.Title.ToLower() == theme.ToLower())
+                .ToList();
 
             return View(model);
         }
--- Forum.Web/Startup.cs
+++ Forum.Web/Startup.cs
@@ -61,6 +61,10 @@ namespace Forum.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "categoriesByTheme",
+                    template: "{theme}/Categories",
+                    defaults: new { controller = "Categories", action = "Index" });
             });
         }
     }
