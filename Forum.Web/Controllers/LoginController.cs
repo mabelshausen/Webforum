5e46880b7d44d91318419626f2247606e81fbfba
Kasperdee <kasperdee@hotmail.com>
2019-05-14 14:37:50 +0200

    Begin form

--- Forum.Web/Controllers/LoginController.cs
+++ Forum.Web/Controllers/LoginController.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Forum.Web.Models;
 
 namespace Forum.Web.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Login()
+        public IActionResult Login(LoginViewModel loginViewModel)
         {
             return View();
         }
 
-        public IActionResult Register()
+        public IActionResult Register(RegisterViewModel registerViewModel)
         {
             return View();
         }
