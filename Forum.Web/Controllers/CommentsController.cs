e39c302cfce2727e6d4881a5e096a05de81dfc20
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-16 10:07:44 +0200

    add commentsindex viewmodel

--- Forum.Web/Controllers/CommentsController.cs
+++ Forum.Web/Controllers/CommentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Forum.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,7 +19,27 @@ namespace Forum.Web.Controllers
 
         public IActionResult Index(string theme, string category, string post)
         {
-            return View();
+            var model = new CommentsIndexVm();
+
+            model.Theme = fc.Themes
+                .Where(t => t.Title.ToLower() == theme.ToLower())
+                .First();
+
+            model.Category = fc.Categories
+                .Where(c => c.Theme == model.Theme)
+                .Where(c => c.Title.ToLower() == category.ToLower())
+                .First();
+
+            model.Post = fc.Posts
+                .Where(p => p.Category == model.Category)
+                .Where(p => p.Title.ToLower() == post.ToLower())
+                .First();
+
+            model.Comments = fc.Comments
+                .Where(c => c.Post == model.Post)
+                .ToList();
+
+            return View(model);
         }
     }
 }
---
+++ Forum.Web/ViewModels/CommentsIndexVm.cs
@@ -0,0 +1,19 @@
+﻿using Forum.Web.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.ViewModels
+{
+    public class CommentsIndexVm
+    {
+        public Theme Theme { get; set; }
+
+        public Category Category { get; set; }
+
+        public Post Post { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
