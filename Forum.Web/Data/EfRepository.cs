248bfcdbdd97c291e4fd38681443ee14bb6932bf
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-05-16 13:02:25 +0200

    add efrepository

---
+++ Forum.Web/Data/EfRepository.cs
@@ -0,0 +1,23 @@
+﻿using Forum.Web.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Data
+{
+    public class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly ForumContext _fc;
+
+        public EfRepository(ForumContext context)
+        {
+            _fc = context;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _fc.Set<T>();
+        }
+    }
+}
