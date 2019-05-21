333cacb35de0c64c2e5266937eab22f0f5d08bf7
Kasperdee <kasperdee@hotmail.com>
2019-05-21 15:14:35 +0200

    Add account creation

--- Forum.Web/Controllers/LoginController.cs
+++ Forum.Web/Controllers/LoginController.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Forum.Web.Models;
+using Forum.Web.Entities;
 
 namespace Forum.Web.Controllers
 {
     public class LoginController : Controller
     {
+
+        private readonly ForumContext _context;
+
+        public LoginController(ForumContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Login(LoginViewModel loginViewModel)
         {
             return View(loginViewModel);
@@ -23,7 +32,21 @@ namespace Forum.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
-            return View(registerViewModel);
+            if (ModelState.IsValid)
+            {
+                var user = new User();
+                user.Username = registerViewModel.Username;
+                user.Password = registerViewModel.Password;
+
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return new RedirectToActionResult("Index", "Home", null);
+            }
+            else
+            {
+                return View(registerViewModel);
+            }
+
         }
     }
 }
