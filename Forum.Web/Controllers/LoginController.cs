e7ede00a3a29e206dd06b8ad8adfd8003e205297
Kasperdee <kasperdee@hotmail.com>
2019-05-23 09:31:24 +0200

    add temporary messages registration

--- Forum.Web/Controllers/LoginController.cs
+++ Forum.Web/Controllers/LoginController.cs
@@ -42,20 +42,24 @@ namespace Forum.Web.Controllers
                 user.Password = hashedPassword;
 
                 var UsernameAlreadyTaken = _context.Users.Where(u => u.Username == user.Username).FirstOrDefault();
+
                 if (UsernameAlreadyTaken is null)
                 {
                     _context.Add(user);
                     await _context.SaveChangesAsync();
+                    TempData[TemporaryMessage.temporaryMessage] = $@"You have successfully been registered as {registerViewModel.Username}. ";
                     return new RedirectToActionResult("Index", "Home", null);
                 }
                 else
                 {
+                    TempData[TemporaryMessage.temporaryMessage] = "There already exists a user with that username. Please enter a different username. ";
                     return View(registerViewModel);
                 }
 
             }
             else
             {
+                TempData[TemporaryMessage.temporaryMessage] = "An error occured while attempting to create your account. Did you correctly fill everything in? ";
                 return View(registerViewModel);
             }
 
