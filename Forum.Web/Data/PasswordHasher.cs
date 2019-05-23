ad7c2702df3caf78649bdc86784b3e2f1ef5322b
Kasperdee <kasperdee@hotmail.com>
2019-05-23 08:52:34 +0200

    add PasswordHasher

---
+++ Forum.Web/Data/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Data
+{
+    public class PasswordHasher
+    {
+        public static string Hashing(string value)
+        {
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding uTF8 = new UTF8Encoding();
+                byte[] data = md5.ComputeHash(uTF8.GetBytes(value));
+                return Convert.ToBase64String(data);
+            }
+        }
+    }
+}
