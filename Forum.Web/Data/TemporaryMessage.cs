edd214e468614fe07d9f4b6e6e63cca4c831e13b
Kasperdee <kasperdee@hotmail.com>
2019-05-23 09:30:51 +0200

    add temporarymessage files

---
+++ Forum.Web/Data/TemporaryMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Data
+{
+    public static class TemporaryMessage
+    {
+        public const string temporaryMessage =  "Temporary message";
+    }
+}
