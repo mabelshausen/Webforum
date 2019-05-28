ea7f12bb9a2179a0ea3ca310e34d64b5cb6dba46
Kasperdee <kasperdee@hotmail.com>
2019-05-28 20:03:14 +0200

    add admin homepage

--- Forum.Web/Areas/Admin/Controllers/HomeController.cs
+++ Forum.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Forum.Web.Entities;
+using Forum.Web.Models;
+using Forum.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Forum.Web.Areas.Admin.Controllers
@@ -9,9 +13,26 @@ namespace Forum.Web.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly IRepository<Theme> _themeRepo;
+
+        public HomeController(IRepository<Theme> themeRepo)
+        {
+            _themeRepo = themeRepo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = new HomeIndexVm();
+
+            model.Themes = _themeRepo.GetAll();
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
