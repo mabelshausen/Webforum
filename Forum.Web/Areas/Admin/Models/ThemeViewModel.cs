6697b67279f16996acb91b132e0a5a79a708aaf7
Kasperdee <kasperdee@hotmail.com>
2019-05-30 18:12:08 +0200

    add ThemeViewModel

---
+++ Forum.Web/Areas/Admin/Models/ThemeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Forum.Web.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Web.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forum.Web.Areas.Admin.Models
+{
+    public class ThemeViewModel
+    {
+        public IEnumerable<Theme> Themes { get; set; }
+
+        [Display(Name = "Enter the title of the theme you want to add")]
+        public Theme Theme { get; set; }
+
+        [Display(Name = "Enter the description of the theme you want to add")]
+        public string Description { get; set; }
+
+    }
+}
