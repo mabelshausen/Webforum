2dcbdfdc698eb7acb3077b01a07559f410f74a00
Haven Gilles <gilles.haven@gmail.com>
2019-05-30 23:34:27 +0200

    Add Vm for profile

---
+++ Forum.Web/ViewModels/ProfileIndexVm.cs
@@ -0,0 +1,19 @@
+﻿using Forum.Web.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.ViewModels
+{
+    public class ProfileIndexVm
+    {
+        public Guid UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
