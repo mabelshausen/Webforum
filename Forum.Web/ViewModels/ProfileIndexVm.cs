10954cdc2ee168b40f43948cf6e77eace3eb2f69
Haven Gilles <gilles.haven@gmail.com>
2019-05-31 04:50:39 +0200

    Update "ProfileIndexVm"

--- Forum.Web/ViewModels/ProfileIndexVm.cs
+++ Forum.Web/ViewModels/ProfileIndexVm.cs
@@ -11,13 +11,6 @@ namespace Forum.Web.ViewModels
     {
         public Guid UserId { get; set; }
 
-        [Display(Name = "Please enter your desired username.")]
-        [Required(ErrorMessage = "A username is required.")]
-        [StringLength(50, MinimumLength = 6, ErrorMessage = "Username must be at least {2} characters long and cannot be longer than {1} characters.")]
-        [RegularExpression(@"^[\w\d.]{5,}$", ErrorMessage = "Username cannot contain spaces or special characters.")]
         public string Username { get; set; }
-
-        public string Password { get; set; }
-
     }
 }
